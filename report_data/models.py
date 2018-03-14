6d2ccc35815e94d2992545b28b21730d927ccb06
VictorGuzman <viaguz@gmail.com>
2018-03-14 17:41:19 -0600

    Add boolean active field for every version.

--- report_data/models.py
+++ report_data/models.py
@@ -5,23 +5,9 @@ from django.db import models
 from django.utils.encoding import smart_text as smart_unicode
 from django.utils.translation import ugettext_lazy as _
 
-class ReportFile(models.Model):
-    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    file = models.FileField(blank=False, null=False, upload_to='report_data/%Y/%m/%d/%H/%M/%S')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    version = models.ManyToManyField(ReportFileVersion)
-
-    class Meta:
-        verbose_name = _("ReportFile")
-        verbose_name_plural = _("ReportFiles")
-        ordering = ('created',)
-
-    def __unicode__(self):
-        return smart_unicode(self.name)
-
 class ReportFileVersion(models.Model):
     version: models.CharField(max_length=100, unique=True, blank=False, null=False)
+    active: models.BooleanField(blank=False, null=False)
 
     class Meta:
         verbose_name = _("ReportFileVersion")
@@ -30,4 +16,17 @@ class ReportFileVersion(models.Model):
     def __unicode__(self):
         return smart_unicode(self.version)
 
+class ReportFile(models.Model):
+    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    file = models.FileField(blank=False, null=False, upload_to='report_data/%Y/%m/%d/%H/%M/%S')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    versions = models.ManyToManyField(ReportFileVersion)
 
+    class Meta:
+        verbose_name = _("ReportFile")
+        verbose_name_plural = _("ReportFiles")
+        ordering = ('created',)
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
