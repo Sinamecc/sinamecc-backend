1deaf0245fa71c0904f411ecf2f10c768538e7ae
VictorGuzman <viaguz@gmail.com>
2018-03-20 22:06:30 -0600

    Add id to ReportFile serializer.

--- report_data/serializers.py
+++ report_data/serializers.py
@@ -5,7 +5,7 @@ from .models import ReportFile, ReportFileVersion
 class ReportFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReportFile
-        fields = ('name', 'created', 'updated')
+        fields = ('id', 'name', 'created', 'updated')
 
 class ReportFileVersionSerializer(serializers.ModelSerializer):
     class Meta:
