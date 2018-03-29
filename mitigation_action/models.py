f0e9a1223ed7128f4c701dc7d21a64c5a9bf8c22
VictorGuzman <viaguz@gmail.com>
2018-03-29 17:28:11 -0600

    Add first version of mitigation action models.

--- mitigation_action/models.py
+++ mitigation_action/models.py
@@ -1,3 +1,136 @@
-from django.db import models
+from __future__ import unicode_literals
+from django.conf import settings
 
-# Create your models here.
+import uuid
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.encoding import smart_text as smart_unicode
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+User =  get_user_model()
+
+class MitigationAction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    strategy_name = models.CharField(max_length=100, blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    purpose = models.CharField(max_length=500, blank=False, null=False)
+    quantitative_purpose = models.CharField(max_length=500, blank=False, null=False)
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    gas_inventory = models.CharField(max_length=100, blank=False, null=False)
+    emissions_source = models.CharField(max_length=100, blank=False, null=False)
+    carbon_sinks = models.CharField(max_length=100, blank=False, null=False)
+    impact_plan = models.CharField(max_length=500, blank=False, null=False)
+    impact = models.CharField(max_length=500, blank=False, null=False)
+    bibliographic_sources = models.CharField(max_length=500, blank=False, null=False)
+    is_international = models.BooleanField(blank=False, null=False)
+    international_participation = models.CharField(max_length=100, blank=False, null=False)
+    sustainability = models.CharField(max_length=100, blank=False, null=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("MitigationAccess")
+        verbose_name_plural = _("MitigationAccesses")
+        ordering = ('created',)
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+class RegistrationType(models.Model):
+    type = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("RegistrationType")
+        verbose_name_plural = _("RegistrationTypes")
+
+    def __unicode__(self):
+        return smart_unicode(self.type)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("Institution")
+        verbose_name_plural = _("Institutions")
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100, blank=False, null=False)
+    job_title = models.CharField(max_length=100, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    phone = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("Contact")
+        verbose_name_plural = _("Contacts")
+
+    def __unicode__(self):
+        return smart_unicode(self.full_name)
+
+class Status(models.Model):
+    status = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("Status")
+        verbose_name_plural = _("Statuses")
+
+    def __unicode__(self):
+        return smart_unicode(self.status)
+
+class ProgressIndicator(models.Model):
+    type = models.CharField(max_length=100, blank=False, null=False)
+    unit = models.CharField(max_length=100, blank=False, null=False)
+    start_date = models.DateField(null=False)
+
+    class Meta:
+        verbose_name = _("ProgressIndicator")
+        verbose_name_plural = _("ProgressIndicators")
+
+    def __unicode__(self):
+        return smart_unicode(self.type)
+
+class Finance(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    source = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("Finance")
+        verbose_name_plural = _("Finances")
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+class IngeiCompliance(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("IngeiCompliance")
+        verbose_name_plural = _("IngeiCompliances")
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+class GeographicScale(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("GeographicScale")
+        verbose_name_plural = _("GeographicScales")
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+class Location(models.Model):
+    geographical_site = models.CharField(max_length=100, blank=False, null=False)
+    is_gis_annexed = models.BooleanField(blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("Location")
+        verbose_name_plural = _("Locations")
+
+    def __unicode__(self):
+        return smart_unicode(self.geographical_site)
