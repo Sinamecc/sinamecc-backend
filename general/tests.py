b86419abe2e475c111b6422435220fe4f3269819
Carlos <carlemanx@gmail.com>
2018-05-13 22:19:58 -0600

    Issue4 (#16)
    * add question UCC
    
    * Add serializer definition
    
    * Add serializer definition
    
    * Corecction of createsuperuser.sh
    
    * delete files * .pyc
    
    * Issue #4
    
    * simplificación de expresión regular
    
    * Test URL
    
    * ignore migrations *.pyc
    
    * Test success
    
    * exclude .pyc
    
    * corrections
    
    * Delete footer
    
    * local.py
    
    * Tab

--- general/tests.py
+++ general/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+import logging
+import logging.config
+from django.template.defaultfilters import slugify
+from django.contrib.auth import get_user_model
+import json
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+client = Client()
+
+class UrlsTest(TestCase):
+    def test_url(self):
+        userList = ['admin', 'admin_1', 'admin-1', 'admin.1', 'admin.1.ab', 'foo_bar_1']
+        for user in userList:
+            self.user = User.objects.get_or_create(username=user)[0]
+            client.force_login(self.user)
+            path = reverse( "get_user_info_by_name" , args=[self.user.username])
+            response = client.get(path)
+            self.assertEqual(response.status_code, 200)
+            assert response
--- general/urls.py
+++ general/urls.py
@@ -3,7 +3,7 @@ from general import views
 
 urlpatterns = [
     url(
-        r'^api/v1/user/(?P<username>[a-zA-Z0-9]+)$',
+        r'^api/v1/user/(?P<username>[A-Za-z0-9\._-]+)$',
         views.get_user_info_by_name,
         name='get_user_info_by_name'
     ),
