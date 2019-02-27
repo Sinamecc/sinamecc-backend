c7ed7aa536bbd5c17e6d6cc452df5b7eb6e9a8d9
Carlos <carlemanx@gmail.com>
2019-02-27 08:29:21 -0600

    First users issue 132 (#137)
    * added new users
    
    * added mail test
    
    * custom Group
    
    * added user with groups
    
    * added new groups

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class CustomGroup(models.Model):
+    
+    group = models.OneToOneField(Group)
+    label = models.CharField(max_length=200, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _("CustomGroup")
+        verbose_name_plural = _("CustomGroups")
