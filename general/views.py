fd519af41ae31942ed117cbc066c655033076e48
Carlos <carlemanx@gmail.com>
2019-03-07 10:52:04 -0600

    Get groups by user  (#128)
    * Get groups by user.
    
    * unit tests
    
    * the group field was changed to the name field
    
    * added new field

--- general/views.py
+++ general/views.py
@@ -1,18 +1,15 @@
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import get_user_model
+from users.services import UserService
 from general.serializers import UserSerializer
+from general.helpers import ViewHelper
+
+service = UserService()
+view_helper = ViewHelper(service)
 
 # Create your views here.
 @api_view(['GET'])
 def get_user_info_by_name(request, username):
-    UserModel = get_user_model()
-    try:
-        user = UserModel.objects.filter(username=username).get()
-    except UserModel.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+    if request.method == "GET":
+        return view_helper.get_one(username)
--- users/services.py
+++ users/services.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, CustomGroup
+from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -13,3 +14,29 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
+class UserService():
+    def __init__(self):
+        self.USER_DOES_NOT_EXIST = "User does not exist."
+
+    def get(self, username):
+        UserModel = get_user_model()
+        try:
+            user = UserModel.objects.filter(username=username).get()
+            result_user = {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "is_active": user.is_active,
+                "groups": [
+                    {
+                        "id": g.id,
+                        "name": g.name,
+                        "label": CustomGroup.objects.get(id=g.id).label
+                    } for g in user.groups.all()
+                ]
+            }
+            result = (True, result_user)
+        except UserModel.DoesNotExist:
+            result = (False, self.USER_DOES_NOT_EXIST)
+        return result
