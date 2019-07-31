af8aea7c94625dd448366c7572fba2067077f010
rvalenciano <ravf.226@gmail.com>
2019-07-30 23:51:27 -0600

    docker: docker is working now

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,12 +1,12 @@
 from .base import *
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
@@ -19,9 +19,8 @@ DEBUG = True
 
 AWS_STORAGE_BUCKET_NAME = 'sinamecc-dev'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_ACCESS_KEY_ID = 'CHANGEME'
-AWS_SECRET_ACCESS_KEY = 'CHANGEME'
-
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -30,3 +29,5 @@ AWS_PRIVATE_MEDIA_LOCATION = 'sinamecc_file_upload'
 PRIVATE_FILE_STORAGE = 'general.storages.PrivateMediaStorage'
 
 AWS_QUERYSTRING_AUTH = True
+
+ALLOWED_HOSTS = ['*']
