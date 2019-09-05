b4cb735ec39ff93f60b899fbd41e1b1f918d0745
calemand <carlemanx@gmail.com>
2019-09-05 16:23:24 -0600

    add permissions

--- mitigation_action/views.py
+++ mitigation_action/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view, parser_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, parser_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework import status
