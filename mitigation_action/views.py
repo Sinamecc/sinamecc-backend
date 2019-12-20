84711d35674977b36097579bae4ee5008b209232
Izcar Muñoz Torrez <grupo-inco@Izcars-MacBook-Pro.local>
2019-12-19 18:31:02 -0600

    added decorators permissions on mitigations actions

--- mitigation_action/views.py
+++ mitigation_action/views.py
@@ -17,6 +17,9 @@ from django.http import HttpResponseRedirect
 service = MitigationActionService()
 view_helper = ViewHelper(service)
 
+
+
+## Auxialar Endpoint
 @api_view(['GET'])
 @parser_classes((MultiPartParser, FormParser, JSONParser,))
 @has_permission_decorator('read_mitigation_action')
@@ -33,6 +36,46 @@ def post_mitigation(request):
         result = view_helper.post(request)
         return result
 
+
+@api_view(['GET'])
+@parser_classes((MultiPartParser, FormParser, JSONParser,))
+@has_permission_decorator('edit_mitigation_action')
+def get_one_mitigation(request, pk, language='en'):
+    if request.method == 'GET':
+        result = view_helper.get_one(pk, language)
+        return result
+
+
+@api_view(['PUT'])
+@parser_classes((MultiPartParser, FormParser, JSONParser,))
+@has_permission_decorator('edit_mitigation_action')
+def put_mitigation(request, pk, language='en'):
+    if request.method == 'PUT':
+        result = view_helper.put(request, pk, language='en')
+        return result
+
+
+@api_view(['PATCH'])
+@parser_classes((MultiPartParser, FormParser, JSONParser,))
+@has_permission_decorator('edit_mitigation_action')
+def patch_mitigation(request, pk, language='en'):
+    if request.method == 'PATCH':
+        result = view_helper.patch(pk, request)
+        return result
+
+
+@api_view(['DELETE'])
+@parser_classes((MultiPartParser, FormParser, JSONParser,))
+@has_permission_decorator('delte_mitigation_action')
+def delete_mitigation(request, pk):
+    if request.method == 'DELETE':
+        result = view_helper.patch(pk)
+        return result
+
+##
+## Endpoint
+##
+ 
 @api_view(['GET', 'POST'])
 @parser_classes((MultiPartParser, FormParser, JSONParser,))
 def get_post_mitigation(request, language='en'):
@@ -45,30 +88,23 @@ def get_post_mitigation(request, language='en'):
     return result
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 @api_view(['GET', 'DELETE', 'PUT', 'PATCH'])
-def get_delete_put_patch_mitigation(request, pk, language):
+@parser_classes((MultiPartParser, FormParser, JSONParser,))
+def get_delete_put_patch_mitigation(request, pk, language='en'):
     if request.method == 'GET':
-        result = view_helper.get_one(pk,language)
+        result = get_one_mitigation(request, pk,language)
     elif request.method == 'DELETE':
-        result = view_helper.delete(pk)
+        result = delete_mitigation(request, pk)
     elif request.method == 'PUT':
-        result = view_helper.put(pk, request, language)
+        result = put_mitigation(request, pk, language)
     elif request.method == 'PATCH':
-        result = view_helper.patch(pk, request)
+        result = patch_mitigation(request, pk, language)
     return result
 
+
+
+
+## review those endpoints
 @api_view(['GET', 'DELETE', 'PUT', 'PATCH'])
 def get_mitigation_change_log(request, pk):
     if request.method == 'GET':
@@ -86,9 +122,6 @@ def get_mitigations_form_es_en(request, language, option):
     if request.method == 'GET':
         result = view_helper.execute_by_name( "get_form_data_es_en", language, option)
     return result
-
-
-
 
 @api_view(['GET'])
 def get_mitigation_action_file(request, id, file_id):
