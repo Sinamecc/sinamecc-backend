61e0769a27b9fdd7ffdd5b044bd86a28c077cef5
Marco Salgado <549503+msalgado@users.noreply.github.com>
2020-04-16 07:50:33 -0600

    Updating Dockerfile and jenkinsfile

--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -12,26 +12,32 @@ pipeline {
                     echo "Step: Updating requirements"
                     sh 'pip install -r requirements.txt'
 
-                    echo "Step: Running Migrations"
-                    sh 'python manage.py migrate'
-
                     echo "Step: Running Tests"
                     sh 'python manage.py test'
+
+                    echo "Step: Running Migrations"
+                    sh 'python manage.py migrate'
                 }
             }
         }
 
         stage ("Building docker image") {
             steps {
-                echo "Step: Build and Test"
+                withPythonEnv('/usr/bin/python3.6') {
+                    echo "Step: Building docker image"
+                    sh 'docker build -t sinamecc_backend:latest .'
+                }
             }   
         }
 
-        stage ("Restarting service") {
+        stage ("Restarting docker container") {
             steps {
-                echo "Step: Build and Test"
+                echo "Step: Stopping current container"
+                sh 'docker stop sinamecc_backend_dev'
+
+                echo "Step: Running new container"
+                sh 'docker run -d -e "DJANGO_SETTINGS_MODULE=config.settings.stage_aws" --name sinamecc_backend_dev sinamecc_backend:latest'
             }   
         }
-
     }
 }
