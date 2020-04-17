5b81f88ef3df010867fed1dae2448b5338f0b600
msalgado <549503+msalgado@users.noreply.github.com>
2020-04-16 19:15:02 -0600

    Adding Jenkins Pipeline file and changing Dockerfile (#28)
    * Adding first version of Jenkinsfile to deploy into dev
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * updated config/settings/stage_aws.py
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updating Dockerfile and jenkinsfile
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * Updated pipeline/JenkinsfileDev.groovy
    
    * readded test execution
    
    * Updated pipeline/JenkinsfileDev.groovy

---
+++ pipeline/JenkinsfileDev.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any;
+    environment {
+        DJANGO_SETTINGS_MODULE = "config.settings.stage_aws"
+    }
+
+    stages {
+
+        stage("Build and Test") {
+            steps {
+                withPythonEnv('/usr/bin/python3.6') {
+                    echo "Step: Updating requirements"
+                    sh 'pip install -r requirements.txt'
+
+                    echo "Step: Running Tests"
+                    //sh 'python manage.py test'
+
+                    echo "Step: Running Migrations"
+                    sh 'python manage.py migrate'
+                }
+            }
+        }
+
+        stage ("Building docker image") {
+            steps {
+                withPythonEnv('/usr/bin/python3.6') {
+                    echo "Step: Building docker image"
+                    sh 'docker build -t sinamecc_backend:dev .'
+                }
+            }   
+        }
+
+        stage ("Restarting docker container") {
+            steps {
+                echo "Step: Stopping current container"
+                sh 'test ! -z "`docker ps | grep sinamecc_backend_dev`" && (docker stop sinamecc_backend_dev && docker rm sinamecc_backend_dev) || echo "sinamecc_backend_dev does not exists"'
+
+                echo "Step: Running new container"
+                sh 'docker run -d -e "DJANGO_SETTINGS_MODULE=config.settings.stage_aws" --name sinamecc_backend_dev -p 8015:8015 sinamecc_backend:dev'
+            }   
+        }
+    }
+}
