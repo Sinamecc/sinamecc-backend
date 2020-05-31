9be4beb6d71b3e1ac691fe0b7b0e84a5ee43e8a4
Marco Salgado <549503+msalgado@users.noreply.github.com>
2020-05-31 15:47:22 -0600

    Update stage_aws.py

--- config/settings/stage_aws.py
+++ config/settings/stage_aws.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = ('stage.sinamecc.go.cr','ec2-18-218-214-170.us-east-2.compute.amazonaws.com','ip-172-31-20-146.us-east-2.compute.internal')
 
-AWS_STORAGE_BUCKET_NAME = 'sinamecc-stage'
+AWS_STORAGE_BUCKET_NAME = 'sinamecc-dev'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
@@ -20,7 +20,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_PRIVATE_MEDIA_LOCATION = 'sinamecc_file_upload'
+AWS_PRIVATE_MEDIA_LOCATION = 'sinamecc_file_upload_stage'
 PRIVATE_FILE_STORAGE = 'general.storages.PrivateMediaStorage'
 
 AWS_QUERYSTRING_AUTH = True
