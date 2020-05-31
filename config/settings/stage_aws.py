419c62d4fe40545cb18ccab693cb1c0a26618d27
Marco Salgado <549503+msalgado@users.noreply.github.com>
2020-05-31 15:44:08 -0600

    Update stage_aws.py

--- config/settings/stage_aws.py
+++ config/settings/stage_aws.py
@@ -11,8 +11,6 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = ('stage.sinamecc.go.cr','ec2-18-218-214-170.us-east-2.compute.amazonaws.com','ip-172-31-20-146.us-east-2.compute.internal')
 
-MEDIA_ROOT = "/var/www/app/sinamecc-uploads"
-
 AWS_STORAGE_BUCKET_NAME = 'sinamecc-stage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
