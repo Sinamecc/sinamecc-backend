2ef76217f1b9ece90753b81aaf9c4e8f9337dba3
Marco Salgado <msalgado@grupoincocr.com>
2021-08-22 19:53:23 -0600

    typo

--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -3,7 +3,7 @@ pipeline {
     environment {
         DJANGO_SETTINGS_MODULE = "config.settings.ecs_aws"
         DATABASE_HOST = "sinamecc-pgsql-dev.copuo03vfifp.us-east-2.rds.amazonaws.com"
-        DATABASE_NAME = "sinameccdbadmin"
+        DATABASE_NAME = "sinamecc"
         DATABASE_CREDS  = credentials('sinamecc-dev-dba')
         DATABASE_URL    = "postgres://${DATABASE_CREDS}@${DATABASE_HOST}:5432/${DATABASE_NAME}"
 
