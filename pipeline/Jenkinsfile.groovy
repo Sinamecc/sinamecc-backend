c9f37d84be1426b935e8cdbec20fb73f24f431b6
Marco Salgado <msalgado@grupoincocr.com>
2021-09-06 22:28:17 -0600

    missing logic for stage/prod deployment

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -13,7 +13,6 @@ pipeline {
         BASE_ECR = "973157324549.dkr.ecr.us-east-2.amazonaws.com"
         ENVIRONMENT = "stage"
         APP = "sinamecc-backend"
-        NGINX_TAG = "nginx-$ENVIRONMENT"
         ECS_CLUSTER_NAME = "sinamecc-cluster-$ENVIRONMENT"
         ECS_SERVICE_NAME = "sinamecc-backend-$ENVIRONMENT"
     }
@@ -64,17 +63,45 @@ pipeline {
           }
         }
 
-        stage("Pushing Image to Prod and Updating Service") {
-          steps {
-            echo "Need to re-tag image"
+        stage("Re-tagging stage images for prod") {
+          environment {
+            ENVIRONMENT = "prod"
+            APP = "sinamecc-backend"
+          }
 
-            echo "Need to push image and update prod"
+          steps {
+            echo "Step: Re-tagging base docker image for web"
+            sh 'docker tag $BASE_ECR/stage/$APP:stage $BASE_ECR/$ENVIRONMENT/$APP:$ENVIRONMENT'
           }
         }
 
-        stage ("Waiting on PRod to be healthy") {
+        stage("Pushing Image to Prod and Updating Service") {
+          environment {
+              ENVIRONMENT = "prod"
+              ECS_CLUSTER_NAME = "sinamecc-cluster-$ENVIRONMENT"
+              ECS_SERVICE_NAME = "sinamecc-backend-$ENVIRONMENT"
+          }
+          steps {
+            echo "Step: Login ECR"
+            sh '/usr/local/bin/aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $BASE_ECR/$ENVIRONMENT/$APP'
+
+            echo "Step: Pushing base image"
+            sh 'docker push $BASE_ECR/$ENVIRONMENT/$APP:$ENVIRONMENT'
+
+            echo "Step: Restarting ECS Service"
+            sh '/usr/local/bin/aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment'
+          }
+        }
+
+        stage ("Waiting on Prod to be healthy") {
+          environment {
+              ENVIRONMENT = "prod"
+              ECS_CLUSTER_NAME = "sinamecc-cluster-$ENVIRONMENT"
+              ECS_SERVICE_NAME = "sinamecc-backend-$ENVIRONMENT"
+          }
           steps {
             echo "Step: Waiting on Service to be healthy"
+            sh '/usr/local/bin/aws ecs wait services-stable --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME'
           }
         }
     }
