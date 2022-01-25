619e71a83b583af8a9793a83326357dc96f0ac7b
Izcar Munoz <izcarmt95@gmail.com>
2022-01-25 12:41:06 -0600

    added url for download files in serializers

--- report_data/serializers.py
+++ report_data/serializers.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework import serializers
 import report_data
 from report_data.models import ReportData, Report, ReportFile, ReportDataChangeLog
@@ -26,11 +27,15 @@ class ReportDataSerializer(serializers.ModelSerializer):
             'contact')
         
 
-            
+    def _get_source_file_url(self, obj):
+        
+        return reverse('get_source_file_to_report_data', kwargs={'report_data_id': obj.id}) if obj.source_file else None 
+       
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['contact'] = ContactSerializer(instance.contact).data
         data['report_data_change_log'] = ReportDataChangeLogSerializer(instance.report_data_change_log.all().order_by('-id'), many=True).data
+        data['source_file'] = self._get_source_file_url(instance)
         data['files'] = ReportFileSerializer(instance.report_file.all(), many=True).data
 
         return data
@@ -46,4 +51,15 @@ class ReportFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReportFile
         fields = ('id', 'slug', 'file', 'report_data', 'report_type')
-
+        
+    def _get_url(self, obj):
+        
+        return reverse('get_file_to_report_data', kwargs={'report_file_id': obj.id})
+    
+    
+    def to_representation(self, instance):
+        
+        data =  super().to_representation(instance)
+        data["file"] = self._get_url(instance)
+        
+        return data
