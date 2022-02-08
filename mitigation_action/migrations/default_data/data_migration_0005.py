4af6527ef1a729d91d43201f64a74efcfb733746
Izcar Munoz <izcarmt95@gmail.com>
2022-02-08 15:44:16 -0600

    First mirgations

--- mitigation_action/migrations/default_data/data_migration_0005.py
+++ mitigation_action/migrations/default_data/data_migration_0005.py
@@ -1,36 +1,36 @@
 _status_data = [
-    {'status':'Planning', 'code': 'planning'},
-    {'status': 'Implementation', 'code': 'implementation'},
-    {'status': 'Completed', 'code': 'completed'}
+    {'status_en':'Planning', 'status_es': 'Planeamiento', 'code': 'planning'},
+    {'status_en': 'Implementation', 'status_es': 'Implementación',  'code': 'implementation'},
+    {'status_en': 'Completed', 'status_es': 'Completado',  'code': 'completed'}
 ]
 
 _finance_status_data = [
-    {'name': 'To obtain', 'code': 'to_obtain'},
-    {'name': 'Guarantee', 'code': 'guarantee'}
+    {'name_en': 'To obtain', 'code': 'to_obtain'},
+    {'name_en': 'Guarantee', 'code': 'guarantee'},
 ]
 
 _initiative_type_data = [
 
-    {'name':'Policy Instrument', 'code': 'policy_instrument', 'type': 'A'},
-    {'name':'Project', 'code': 'project', 'type': 'B'},
-    {'name':'Activity', 'code': 'activity' ,'type': 'C'}
+    {'name_en':'Policy Instrument', 'name_es': '', 'code': 'policy_instrument', 'type': 'A'},
+    {'name_en':'Project', 'name_es': '', 'code': 'project', 'type': 'B'},
+    {'name_en':'Activity', 'name_es': '', 'code': 'activity' ,'type': 'C'}
 ]
 
 _finance_source_type_data = [
-    {'name':'Public Budget', 'code': 'public_budget'},
-    {'name':'Private Finance', 'code': 'private_finance'},
-    {'name':'Cooperation Project', 'code': 'cooperation_project'},
-    {'name': 'Loan', 'code': 'loan'},
-    {'name':'To define', 'code': 'to_define'},
+    {'name_en':'Public Budget', 'name_es': '', 'code': 'public_budget'},
+    {'name_en':'Private Finance', 'name_es': '', 'code': 'private_finance'},
+    {'name_en':'Cooperation Project', 'name_es': '', 'code': 'cooperation_project'},
+    {'name_en': 'Loan', 'name_es': '', 'code': 'loan'},
+    {'name_en':'To define', 'name_es': '', 'code': 'to_define'},
 ]
 
 _geographic_scale_data = [
-    {'name':'National', 'code': 'national'},
-    {'name':'Regional', 'code': 'regional'},
-    {'name':'Local', 'code': 'local'},
+    {'name_en':'National', 'name_es': '', 'code': 'national'},
+    {'name_en':'Regional', 'name_es': '', 'code': 'regional'},
+    {'name_en':'Local', 'name_es': '', 'code': 'local'},
 ]
 
 _impact_category_data = [
-    {'name': 'Direct impact on GHG', 'code': 'direct_impact_on_ghg'},
-    {'name': 'Enabling measure', 'code': 'enabling_measure'},
+    {'name_en': 'Direct impact on GHG', 'name_es': '', 'code': 'direct_impact_on_ghg'},
+    {'name_en': 'Enabling measure', 'name_es': '', 'code': 'enabling_measure'},
 ]
