08b3ebb2e1dd54a44932623d106368ecd9afa6cf
Izcar Munoz <izcarmt95@gmail.com>
2022-03-10 16:17:13 -0600

    clean migration: Change some field of report data model

--- report_data/models.py
+++ report_data/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils.encoding import smart_text as smart_unicode
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
-from mitigation_action.models import Contact, ThematicCategorizationType, Classifier
+from mitigation_action.models import Contact, ThematicCategorizationType, Classifier, InformationSourceType
 from general.storages import PrivateMediaStorage
 from general.utils import unique_field_value_generator
 from time import gmtime, strftime
@@ -35,31 +35,62 @@ class ReportData(models.Model):
         ('url', _('url')),
     )
 
+    PERIODICITY = (
+        ('yearly', 'Yearly'),
+        ('biannual', 'Biannual'),
+        ('quartely', 'Quartely')
+    )
+    
+    GEOGRAPHIC = (
+        ('NATIONAL', 'National'),
+        ('REGIONAL', 'Regional'),
+        ('PROVINCIAL', 'Provincial'),
+        ('CANTONAL', 'Cantonal'),
+        ('OTHER', 'Other')
+    )
+    fsm_state = FSMField(default='new', protected=True, max_length=100)
     user = models.ForeignKey(User, related_name='report_data', on_delete=models.CASCADE)
+    
+    # sec 2
+    ###
     name = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
-    source = models.TextField(blank=True, null=True)
-    source_file = models.FileField(upload_to=directory_path, storage=PrivateMediaStorage(), blank=True, null=True)
-    data_type = models.ForeignKey(ThematicCategorizationType, related_name='report_data', null=True, on_delete=models.CASCADE)
-    other_data_type = models.CharField(max_length=255, blank=True, null=True)
-    classifier = models.ForeignKey(Classifier, related_name='report_data', null=True, on_delete=models.CASCADE)
-    other_classifier = models.CharField(max_length=255, blank=True, null=True)
-    report_information = models.CharField(max_length=100, choices=REPORT_TYPE_CHOICES, blank=False, null=False)
+    unit = models.CharField(max_length=100, blank=True, null=True)
+    calculation_methodology = models.CharField(max_length=1000, blank=True, null=True)
+    measurement_frequency = models.CharField(max_length=100, choices=PERIODICITY, blank=True, null=True)
+    ## available time field
+    from_date = models.DateField(blank=True, null=True)
+    to_date = models.DateField(blank=True, null=True)
+    geographic_coverage = models.CharField(max_length=255, choices=GEOGRAPHIC, null=True)
+    disaggregation = models.TextField(null=True)
+    limitation = models.TextField(null=True)
+    additional_information = models.TextField(null=True)
+    ## information source 
+    information_source = models.ForeignKey(InformationSourceType, related_name='report_data', on_delete=models.DO_NOTHING, null=True)
+    statistical_operation = models.TextField(null=True)
+    contact = models.ForeignKey(Contact, related_name='report_data', null=True, on_delete=models.CASCADE)
+    contact_annotation = models.TextField(null=True)
     
+    ## Categorization of report data
+    data_type = models.ForeignKey(ThematicCategorizationType, related_name='report_data', null=True, on_delete=models.DO_NOTHING)
+    other_data_type = models.CharField(max_length=255, blank=True, null=True)
+    classifier = models.ManyToManyField(Classifier, related_name='report_data')
+    other_classifier = models.CharField(max_length=255, blank=True, null=True)
+    ###
+    # sec 1.2 
+    report_information = models.CharField(max_length=100, choices=REPORT_TYPE_CHOICES, blank=False, null=False)
     have_line_base = models.BooleanField(default=False)
     line_base_type = models.CharField(max_length=100,choices=REPORT_DATA_TYPE_CHOICES, blank=True, null=True)
     line_base_report = models.TextField(blank=True, null=True) ## can be used for the line base report or url 
-    
     have_quality_element = models.BooleanField(default=False)
     quality_element_description = models.TextField(blank=True, null=True)
     transfer_data_with_sinamecc = models.BooleanField(default=False)
     transfer_data_with_sinamecc_description = models.TextField(blank=True, null=True)
-    
+    #attr report_data -> File in the report_data Report File model
     report_data_type = models.CharField(max_length=100, choices=REPORT_DATA_TYPE_CHOICES, blank=True, null=True)
     individual_report_data = models.TextField(blank=True, null=True) ## can be used for the individual report or url
     
-    contact = models.ForeignKey(Contact, related_name='report_data', null=True, on_delete=models.CASCADE)
-
+    
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -71,76 +102,8 @@ class ReportData(models.Model):
     def __unicode__(self):
         return smart_unicode(self.name)
 
-
-class Report(models.Model):
-    user = models.ForeignKey(User, related_name='report', on_delete=models.CASCADE)
-    report_data = models.ForeignKey(ReportData, related_name='report', on_delete=models.CASCADE)
-    version = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    active = models.BooleanField(blank=False, null=False)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = _("Report")
-        verbose_name_plural = _("Report")
     
-    def __unicode__(self):
-        return smart_unicode(self.version)
-
-
-class ReportFile(models.Model):
-
-    slug = models.SlugField(max_length=100, unique=True, blank=False, null=False)
-    file = models.FileField(upload_to=directory_path, storage=PrivateMediaStorage(), blank=True, null=True)
-    report_data = models.ForeignKey(ReportData, related_name='report_file', on_delete=models.CASCADE, null=True)
-    report_type = models.CharField(max_length=100, null=True) ## base_line_indicator | report_data
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = _("Report File")
-        verbose_name_plural = _("Report Files")
-
-    def _create_slug(self):
-        symbols = string.ascii_letters + '-'
-        return unique_field_value_generator(self, 20, 'slug', symbols)
-
-
-    def save(self, *args, **kwargs):
-
-        if not self.slug:
-            self.slug = self._create_slug()
-        
-        super(ReportFile, self).save(*args, **kwargs)
-
-
-class ReportFileMetadata(models.Model):
-
-    name = models.CharField(max_length=100, blank=False, null=False)
-    value = models.CharField(max_length=100, blank=False, null=False)
-    report_file = models.ForeignKey(ReportFile, on_delete=models.CASCADE)
-    fsm_state = FSMField(default='new', protected=True, max_length=100)
-
-    class Meta:
-        verbose_name = _("Report File Metadata")
-        verbose_name_plural = _("Report FileMetadata")
-
-
-class ReportDataChangeLog(models.Model):
-
-    report_data = models.ForeignKey(ReportData, related_name='report_data_change_log', null=True, blank=True, on_delete=models.CASCADE)
-    changes = models.TextField(null=True, blank=True)
-    change_description = models.TextField(null=True, blank=True)
-    author = models.ForeignKey(User, related_name='report_data_change_log', on_delete=models.CASCADE)
-    updated = models.DateTimeField(auto_now=True)
-
-
-    class Meta:
-        verbose_name = _("Report Data Changelog")
-        verbose_name_plural = _("Report Data Changelogs")
-
-    # FSM Annotated Methods (Transitions) and Ordinary Conditions
+        # FSM Annotated Methods (Transitions) and Ordinary Conditions
     # --- Transition ---
     def can_submit(self):
         ## check all field are filled !!
@@ -233,3 +196,60 @@ class ReportDataChangeLog(models.Model):
         # send email to user that submitted the action
         print('The report data is transitioning from <accepted_by_DCC> to <registered_by_DCC>')
         ...
+
+
+class Report(models.Model):
+    user = models.ForeignKey(User, related_name='report', on_delete=models.CASCADE)
+    report_data = models.ForeignKey(ReportData, related_name='report', on_delete=models.CASCADE)
+    version = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    active = models.BooleanField(blank=False, null=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Report")
+        verbose_name_plural = _("Report")
+    
+    def __unicode__(self):
+        return smart_unicode(self.version)
+
+
+class ReportFile(models.Model):
+
+    slug = models.SlugField(max_length=100, unique=True, blank=False, null=False)
+    file = models.FileField(upload_to=directory_path, storage=PrivateMediaStorage(), blank=True, null=True)
+    report_data = models.ForeignKey(ReportData, related_name='report_file', on_delete=models.CASCADE, null=True)
+    report_type = models.CharField(max_length=100, null=True) ## base_line_indicator | report_data
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Report File")
+        verbose_name_plural = _("Report Files")
+
+    def _create_slug(self):
+        symbols = string.ascii_letters + '-'
+        return unique_field_value_generator(self, 20, 'slug', symbols)
+
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = self._create_slug()
+        
+        super(ReportFile, self).save(*args, **kwargs)
+
+class ReportDataChangeLog(models.Model):
+
+    report_data = models.ForeignKey(ReportData, related_name='report_data_change_log', null=True, blank=True, on_delete=models.CASCADE)
+    changes = models.TextField(null=True, blank=True)
+    change_description = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(User, related_name='report_data_change_log', on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = _("Report Data Changelog")
+        verbose_name_plural = _("Report Data Changelogs")
+
--- report_data/serializers.py
+++ report_data/serializers.py
@@ -21,21 +21,13 @@ class ReportDataChangeLogSerializer(serializers.ModelSerializer):
 class ReportDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReportData
-        fields = ('id', 'user', 'name', 'description', 'source', 'source_file', 'data_type', 'other_data_type', 
-            'classifier', 'other_classifier', 'report_information', 'have_line_base', 'line_base_type', 'line_base_report', 'have_quality_element', 
-            'quality_element_description', 'transfer_data_with_sinamecc', 'report_data_type', 'individual_report_data', 'transfer_data_with_sinamecc_description', 
-            'contact')
+        fields = ('__all__')
         
-
-    def _get_source_file_url(self, obj):
-        
-        return reverse('get_source_file_to_report_data', kwargs={'report_data_id': obj.id}) if obj.source_file else None 
-       
+ 
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['contact'] = ContactSerializer(instance.contact).data
         data['report_data_change_log'] = ReportDataChangeLogSerializer(instance.report_data_change_log.all().order_by('-id'), many=True).data
-        data['source_file'] = self._get_source_file_url(instance)
         data['files'] = ReportFileSerializer(instance.report_file.all(), many=True).data
 
         return data
