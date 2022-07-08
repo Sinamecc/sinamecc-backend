38f2d1b4dee97cd220111a49e7ad002c062f290f
Izcar Munoz <izcarmt95@gmail.com>
2022-07-08 12:16:31 -0600

    Fixed

--- mitigation_action/migrations/0020_auto_20220607_2131.py
+++ mitigation_action/migrations/0020_auto_20220607_2131.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mitigation_action', '0019_auto_20220503_0140'),
+        ('mitigation_action', '0019_auto_20220503_01401'),
     ]
 
     operations = [
