d810aece4720a8f998fa35af01c74b237af2f90e
Gerarld <gerarld73@gmail.com>
2022-09-08 14:56:12 -0600

    Update address model

--- general/serializers.py
+++ general/serializers.py
@@ -46,11 +46,12 @@ class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Address
-        fields = ('id', 'app_scale', 'description', 'GIS', 'district', 'created', 'updated')
+        fields = ('id', 'app_scale', 'description', 'GIS', 'district', 'canton', 'created', 'updated')
     
     def to_representation(self, instance):
 
         data = super().to_representation(instance)
         data['district'] = DistrictSerializer(instance.district.all(), many=True).data
+        data['canton'] = CantonSerializer(instance.canton.all(), many=True).data
 
         return data
