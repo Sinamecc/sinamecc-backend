d4895dae8cc793fa39716d139e3f7a5708446c02
Izcar J. Muñoz-Torrez <izcarmt95@gmail.com>
2022-09-26 12:00:36 -0600

    Mitigation Action: New Version (#87)
    * Added code AM
    
    * Added endpoint for sending to review
    
    * Added section 2 finance
    
    * Added changes section 5
    
    * fixed year
    
    * Mitigation Section 1: Multiple selection Areas NDC (#86)
    
    * Added Action Area
    
    * Added Topic
    
    * Added new selection nested
    
    * Fixed comments
     and added models for selection section #4
    
    * Fixed serializer
    
    * Fixed migrations
    
    * Fixed Internacionalizacion
    
    * Added sectors
    
    * Add catalogs for sector IPCC
    
    * Add enpoints for create sector selection
    
    * Fix labes

--- mitigation_action/migrations/default_data/data_migration_0015.py
+++ mitigation_action/migrations/default_data/data_migration_0015.py
@@ -1,71 +1,88 @@
 _sustainable_development_goals = [
     {
         'code': '1',
-        'description': 'Poner fin a la pobreza en todas sus formas en todo el mundo',
+        'description_es': 'Poner fin a la pobreza en todas sus formas en todo el mundo',
+        'description_en': 'End poverty in all its forms in all the world',
     },
     {
         'code': '2',
-        'description': 'Poner fin al hambre',
+        'description_es': 'Poner fin al hambre',
+        'description_en': 'End hunger',
     },
     {
         'code': '3',
-        'description': 'Garantizar una vida sana y promover el bienestar para todos en todas las edades',
+        'description_es': 'Garantizar una vida sana y promover el bienestar para todos en todas las edades',
+        'description_en': 'Ensure a healthy life and promote health for all in all ages',
     },
     {
         'code': '4',
-        'description': 'Garantizar una educación inclusiva, equitativa y de calidad y promover oportunidades de aprendizaje durante toda la vida para todos',
+        'description_es': 'Garantizar una educación inclusiva, equitativa y de calidad y promover oportunidades de aprendizaje durante toda la vida para todos',
+        'description_en': 'Ensure an inclusive, equitable and quality education and promote opportunities of learning throughout the life for all',
     },
     {
         'code': '5',
-        'description': 'Lograr la igualdad entre los géneros y empoderar a todas las mujeres y las niñas',
+        'description_es': 'Lograr la igualdad entre los géneros y empoderar a todas las mujeres y las niñas',
+        'description_en': 'Achieve gender equality and empower all women and girls'
     },
     {
         'code': '6',
-        'description': 'Garantizar la disponibilidad de agua y su gestión sostenible y el saneamiento para todos',
+        'description_es': 'Garantizar la disponibilidad de agua y su gestión sostenible y el saneamiento para todos',
+        'description_en': 'Guarantee the availability of water and its sustainable management and sanitation for all'
     },
     {
         'code': '7',
-        'description': 'Garantizar el acceso a una energía asequible, segura, sostenible y moderna',
+        'description_es': 'Garantizar el acceso a una energía asequible, segura, sostenible y moderna',
+        'description_en': 'Ensure access to an affordable, secure, sustainable and modern energy'
     },
     {
         'code': '8',
-        'description': 'Promover el crecimiento económico inclusivo y sostenible, el empleo y el trabajo decente para todos',
+        'description_es': 'Promover el crecimiento económico inclusivo y sostenible, el empleo y el trabajo decente para todos',
+        'description_en': 'Promote economic growth inclusive and sustainable, employment and decent work for all'
     },
     {
         'code': '9',
-        'description': 'Construir infraestructuras resilientes, promover la industrialización sostenible y fomentar la innovación',
+        'description_es': 'Construir infraestructuras resilientes, promover la industrialización sostenible y fomentar la innovación',
+        'description_en': 'Build resilient infrastructure, promote sustainable industrialization and promote innovation'
     },
     {
         'code': '10',
-        'description': 'Reducir la desigualdad en y entre los países',
+        'description_es': 'Reducir la desigualdad en y entre los países',
+        'description_en': 'Reduce inequality in and between countries'
     },
     {
         'code': '11',
-        'description': 'Lograr que las ciudades sean más inclusivas, seguras, resilientes y sostenibles',
+        'description_es': 'Lograr que las ciudades sean más inclusivas, seguras, resilientes y sostenibles',
+        'description_en': 'Achieve cities more inclusive, secure, resilient and sustainable'
     },
     {
         'code': '12',
-        'description': 'Garantizar modalidades de consumo y producción sostenibles',
+        'description_es': 'Garantizar modalidades de consumo y producción sostenibles',
+        'description_en': 'Ensure sustainable consumption and production modes'
     },
     {
         'code': '13',
-        'description': 'Adoptar medidas urgentes para combatir el cambio climático y sus efectos',
+        'description_es': 'Adoptar medidas urgentes para combatir el cambio climático y sus efectos',
+        'description_en': 'Adopt measures to combat climate change and its effects'
     },
     {
         'code': '14',
-        'description': 'Conservar y utilizar sosteniblemente los océanos, los mares y los recursos marinos',
+        'description_es': 'Conservar y utilizar sosteniblemente los océanos, los mares y los recursos marinos',
+        'description_en': 'Preserve and use sustainable oceans, seas and marine resources'
     },
     {
         'code': '15',
-        'description': 'Gestionar sosteniblemente los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras, detener la pérdida de biodiversidad',
+        'description_es': 'Gestionar sosteniblemente los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras, detener la pérdida de biodiversidad',
+        'description_en': 'Manage sustainable forests, fight against desertification, stop and invest in the degradation of the lands, stop the loss of biodiversity'
     },
     {
         'code': '16',
-        'description': 'Promover sociedades justas, pacíficas e inclusivas',
+        'description_es': 'Promover sociedades justas, pacíficas e inclusivas',
+        'description_en': 'Promote just, peaceful and inclusive societies'
     },
     {
         'code': '17',
-        'description': 'Revitalizar la Alianza Mundial para el Desarrollo Sostenible',
+        'description_es': 'Revitalizar la Alianza Mundial para el Desarrollo Sostenible',
+        'description_en': 'Revitalize the Global Partnership for Sustainable Development'
     }
 ]
 
@@ -73,19 +90,28 @@ _sustainable_development_goals = [
 _ghg_impact_sector = [
     {
         'code': '1',
-        'name': 'Energía',
+        'name_es': 'Energía',
+        'name_en': 'Energy',
     },
     {
         'code': '2',
-        'name': 'Agricultura, Silvicultura y Otros Usos de la Tierra (AFOLU)',
+        'name_es': 'Agricultura, Silvicultura y Otros Usos de la Tierra (AFOLU)',
+        'name_en': 'Agriculture, forestry and other land uses (AFOLU)',
     },
     {
         'code': '3',
-        'name': 'Procesos Industriales y Uso de Productos (IPPU)',
+        'name_es': 'Procesos Industriales y Uso de Productos (IPPU)',
+        'name_en': 'Industrial processes and use of products (IPPU)',
     },
     {
         'code': '4',
-        'name': 'Residuos',
+        'name_es': 'Residuos',
+        'name_en': 'Waste',
+    },
+    {
+        'code': '5',
+        'name_es': 'Uso de la tierra, cambio del uso de la tierra y silvicultura (UTCUTS)',
+        'name_en': 'Land use, change in land use and forestry (UTCUTS)',
     }
 ]
 
--- mitigation_action/serializers.py
+++ mitigation_action/serializers.py
@@ -1,13 +1,12 @@
-from django.db.models import manager
-from general import models
-from django.utils.translation import override
 from rest_framework import serializers
 from mitigation_action.models import ActionAreas, ActionGoals, Finance, MitigationAction, Contact, Status, FinanceSourceType, GeographicScale,\
     InitiativeType, FinanceStatus, InitiativeGoal, Initiative, MitigationActionStatus, GeographicLocation, GHGInformation, \
     ImpactDocumentation, QAQCReductionEstimateQuestion, Indicator, MonitoringInformation, MonitoringIndicator, MonitoringReportingIndicator, \
     ActionAreas, ActionGoals, DescarbonizationAxis, TransformationalVisions, Topics, SubTopics, Activity,  ImpactCategory, Categorization, SustainableDevelopmentGoals, \
-    GHGImpactSector, CarbonDeposit, Standard, ChangeLog,InformationSource, InformationSourceType, ThematicCategorizationType, Classifier, IndicatorChangeLog
-
+    GHGImpactSector, CarbonDeposit, Standard, InformationSource, InformationSourceType, ThematicCategorizationType, Classifier, IndicatorChangeLog, \
+    FinanceInformation, ActionAreasSelection, TopicsSelection, ChangeLog, DescarbonizationAxisSelection, Sector, SectorIPCC2006, CategoryIPCC2006, SubCategoryIPCC2006, SectorSelection
+from workflow.serializers import CommentSerializer
+from general.utils import get_translation_from_database as _
 ##
 ## Auxiliar Class Serializer
 ##
@@ -50,122 +49,324 @@ class GenericListSerializer(serializers.ListSerializer):
 ##
 ## Start Catalogs Serializers
 ##
+
 class SustainableDevelopmentGoalsSerializer(serializers.ModelSerializer):
+    
+    description = serializers.SerializerMethodField()
+    
     class Meta:
         model = SustainableDevelopmentGoals
         fields = ('id', 'code', 'description')
 
+    def get_description(self, instance):
+        return _(instance, 'description')
+
 
 class GHGImpactSectorSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = GHGImpactSector
         fields = ('id', 'code', 'name')
+        
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
 
 
 class CarbonDepositSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = CarbonDeposit
         fields = ('id', 'code', 'name')
+        
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
 
 
 class StandardSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = Standard
         fields = ('id', 'code', 'name')
 
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
+
 
 class ActionAreasSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = ActionAreas
         fields = ('id', 'name', 'code')
 
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
 
 class ActionGoalsSerializer(serializers.ModelSerializer):
+    
+    goal = serializers.SerializerMethodField()
+    
     class Meta:
         model = ActionGoals
         fields = ('id', 'goal', 'code', 'area')
 
+    
+    def get_goal(self, instance):
+        return _(instance, 'goal')
+    
 
 class DescarbonizationAxisSerializer(serializers.ModelSerializer):
+    
+    description = serializers.SerializerMethodField()
+    
     class Meta:
         model = DescarbonizationAxis
         fields = ('id', 'description', 'code')
 
+    def  get_description(self, instance):
+        return _(instance, 'description')
+    
+    
 
 class TransformationalVisionsSerializer(serializers.ModelSerializer):
+    
+    description = serializers.SerializerMethodField()
+    
     class Meta:
         model = TransformationalVisions
         fields = ('id', 'description', 'code', 'axis')
 
+    
+    def get_description(self, instance):
+        return _(instance, 'description')
+    
 
 class TopicsSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = Topics
         fields = ('id', 'name', 'code')
 
+
+    def get_name(self, instance):
+        return _(instance, 'name')
+
+
 class SubTopicsSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = SubTopics
         fields = ('id', 'name', 'code', 'topic')
 
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
+
 
 class ImpactCategorySerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = ImpactCategory
         fields = ('id', 'code', 'name')
 
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
 
 class ActivitySerializer(serializers.ModelSerializer):
+    
+    description = serializers.SerializerMethodField()
+    
     class Meta:
         model = Activity
         fields = ('id', 'description', 'code', 'sub_topic')
 
+    def  get_description(self, instance):
+        return _(instance, 'description')
+    
 
 class InitiativeTypeSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = InitiativeType
         fields = ('id', 'code', 'name', 'type')
 
 
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
+
 class GeographicScaleSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = GeographicScale
         fields = ('id', 'code', 'name')
 
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
 
 class FinanceSourceTypeSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = FinanceSourceType
         fields = ('id', 'code','name')
 
+    def get_name(self, instance):
+        return _(instance, 'name')
+
 
 class FinanceStatusSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = FinanceStatus
         fields = ('id', 'code', 'name')
 
 
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
+    
 class StatusSerializer(serializers.ModelSerializer):
+    
+    status = serializers.SerializerMethodField()
+    
     class Meta:
         model = Status
         fields = ('id', 'code', 'status')
 
+    def get_status(self, instance):
+        return _(instance, 'status')
+    
+    
 
 class InformationSourceTypeSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = InformationSourceType
         fields = ('id', 'code', 'name')
+    
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
 
 
 class ThematicCategorizationTypeSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = ThematicCategorizationType
         fields = ('id', 'code', 'name')
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
 
 class ClassifierSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
     class Meta:
         model = Classifier
         fields = ('id', 'code', 'name')
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
+        
+        
+ 
+class SectorSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Sector
+        fields = ('id', 'code', 'name')
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
+
+
+class SectorIPCC2006Serializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = SectorIPCC2006
+        fields = ('id', 'code', 'name', 'sector')
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
+
+
+class CategoryIPCC2006Serializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = CategoryIPCC2006
+        fields = ('id', 'code', 'name', 'sector_ipcc_2006')
+        
+    def get_name(self, instance):
+        return _(instance, 'name')
+
+
+class SubCategoryIPCC2006Serializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = SubCategoryIPCC2006
+        fields = ('id', 'code', 'name', 'category_ipcc_2006')
+    
+    def get_name(self, instance):
+        return _(instance, 'name')
+    
+
+class SectorSelectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = SectorSelection
+        fields = ('id', 'sector', 'sector_ipcc_2006', 'category_ipcc_2006',
+                  'sub_category_ipcc_2006',  'impact_documentation')
+        list_serializer_class = GenericListSerializer
+    
+    def to_representation(self, instance):
+        data =  super().to_representation(instance)
+        data['sector'] = SectorSerializer(instance.sector).data
+        data['sector_ipcc_2006'] = SectorIPCC2006Serializer(instance.sector_ipcc_2006).data
+        data['category_ipcc_2006'] = CategoryIPCC2006Serializer(instance.category_ipcc_2006).data
+        data['sub_category_ipcc_2006'] = SubCategoryIPCC2006Serializer(instance.sub_category_ipcc_2006.all(), many=True).data
+
+        return data
         
 ##
 ## Finish Catalogs Serializers
@@ -176,20 +377,44 @@ class ClassifierSerializer(serializers.ModelSerializer):
 ## Start Model Serializers
 ##
 ## Edit this serializers
+
+class DescarbonizationAxisSelectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = DescarbonizationAxisSelection
+        fields = ('id', 'descarbonization_axis', 'categorization', 'transformational_vision')
+        list_serializer_class = GenericListSerializer
+    
+    
+class ActionAreasSelectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = ActionAreasSelection
+        fields = ('id', 'area', 'goals', 'categorization')
+        list_serializer_class = GenericListSerializer
+       
+
+class TopicsSelectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = TopicsSelection
+        fields = ('id', 'topic', 'sub_topic', 'categorization')
+        list_serializer_class = GenericListSerializer
+        
+
 class CategorizationSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Categorization
-        fields = ('id', 'action_goal', 'transformational_vision', 'sub_topics', 'activities', 'impact_categories', 'is_part_to_another_mitigation_action', 'relation_description')
+        fields = ('id', 'action_area_selection', 'topics_selection', 'descarbonization_axis_selection', 'impact_category', 'is_part_to_another_mitigation_action', 'relation_description')
     
     def to_representation(self, instance):
         
         data = super().to_representation(instance)
-        data['action_goal'] = ActionGoalsSerializer(instance.action_goal.all(), many=True).data
-        data['transformational_vision'] = TransformationalVisionsSerializer(instance.transformational_vision.all(), many=True).data
-        data['sub_topics'] = SubTopicsSerializer(instance.sub_topics.all(), many=True).data
-        data['activities'] = ActivitySerializer(instance.activities.all(), many=True).data
-        data['impact_categories'] = ImpactCategorySerializer(instance.impact_categories.all(), many=True).data
+        data['action_area_selection'] = ActionAreasSelectionSerializer(instance.action_area_selection.all(), many=True).data
+        data['topics_selection'] = TopicsSelectionSerializer(instance.topics_selection.all(), many=True).data
+        data['descarbonization_axis_selection'] = DescarbonizationAxisSelectionSerializer(instance.descarbonization_axis_selection.all(), many=True).data
+        data['impact_category'] = ImpactCategorySerializer(instance.impact_category).data
 
         return data
 
@@ -225,8 +450,8 @@ class IndicatorSerializer(serializers.ModelSerializer):
         model = Indicator
 
         fields = ('id', 'name', 'description', 'unit', 'methodological_detail', 'methodological_detail_file', 'reporting_periodicity', 'available_time_start_date', 'available_time_end_date', 
-                'geographic_coverage', 'other_geographic_coverage', 'disaggregation', 'limitation', 'additional_information', 'additional_information_file', 'comments',
-                'information_source', 'type_of_data', 'other_type_of_data', 'classifier', 'other_classifier', 'contact', 'monitoring_information')
+                'geographic_coverage', 'other_geographic_coverage', 'disaggregation', 'limitation', 'comments',
+                'information_source', 'type_of_data', 'other_type_of_data', 'contact', 'monitoring_information')
     
 
     def to_representation(self, instance):
@@ -297,6 +522,7 @@ class ImpactDocumentationSerializer(serializers.ModelSerializer):
         data['carbon_deposit'] = CarbonDepositSerializer(instance.carbon_deposit).data
         data['standard'] =  StandardSerializer(instance.standard).data
         data['estimate_calculation_documentation_file'] = self._get_estimate_calculation_documentation_file_url(instance)
+        data['sector_selection'] = SectorSelectionSerializer(instance.sector_selection.all(), many=True).data
 
         return data
 
@@ -329,9 +555,25 @@ class GHGInformationSerializer(serializers.ModelSerializer):
 class FinanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Finance
-        fields = ('id', 'status', 'administration', 'source', 'source_description', 'reference_year', 'budget',
-                    'currency', 'mideplan_registered', 'mideplan_project', 'executing_entity')
+        fields = ('id', 'status', 'administration', 'source', 'reference_year',
+                  'mideplan_registered', 'mideplan_project', 'executing_entity')
+        
+        
+    def to_representation(self, instance):
+        
+        data = super().to_representation(instance)
+        data['finance_information'] = FinanceInformationSerializer(instance.finance_information.all(), many=True).data
+        
+        return data
 
+
+class FinanceInformationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = FinanceInformation
+        fields = ('id', 'source_description', 'budget', 'currency', 'finance')
+        list_serializer_class = GenericListSerializer
+        
 
 class InitiativeGoalSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
@@ -430,8 +672,8 @@ class ChangeLogSerializer(serializers.ModelSerializer):
 class MitigationActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MitigationAction
-        fields = ('id', 'fsm_state','contact', 'initiative', 'status_information', 'geographic_location', 'categorization','finance', 
-                    'ghg_information', 'impact_documentation', 'monitoring_information', 'monitoring_reporting_indicator', 'review_count', 
+        fields = ('id', 'fsm_state','contact', 'code', 'initiative', 'status_information', 'geographic_location', 'categorization','finance', 
+                    'ghg_information', 'impact_documentation', 'monitoring_information', 'monitoring_reporting_indicator', 'review_count', 'comments',
                     'user', 'created', 'updated')
         
         # only read code kwargs
@@ -474,6 +716,7 @@ class MitigationActionSerializer(serializers.ModelSerializer):
         data['impact_documentation'] = ImpactDocumentationSerializer(instance.impact_documentation).data
         data['monitoring_information'] = MonitoringInformationSerializer(instance.monitoring_information).data
         data['monitoring_reporting_indicator'] = MonitoringReportingIndicatorSerializer(instance.monitoring_reporting_indicator).data
+        data['comments'] = CommentSerializer(instance.comments.filter(fsm_state=instance.fsm_state, review_number=instance.review_count), many=True).data
         data['change_log'] = ChangeLogSerializer(instance.change_log.all().order_by('-date')[0:5], many=True).data
         
         return data
