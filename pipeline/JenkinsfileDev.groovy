d2365c9ffb3d8f2a9b4413a5c8ca7f1cb14064eb
Izcar Munoz <izcarmt95@gmail.com>
2022-10-14 16:00:40 -0600

    Fix migrations jenkins

--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -31,7 +31,7 @@ pipeline {
                   sh 'pip install -r requirements.txt'
 
                   echo "Step: Running Migrations"
-                  sh 'python manage.py migrate mitigation_action 0024_auto_20220920_2100 '
+                  sh 'python manage.py migrate'
                 }
             }
         }
