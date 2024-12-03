ab881ba2beed53951ca3615e6decf01254e20b14
Izcar J. Muñoz-Torrez <izcar@grupoincocr.com>
2024-12-03 14:45:42 -0600

    [SIN-i7] fix: clean up settings and dependencies (#126)
    * feat: clean up configuration
    
    * fix: update github action
    
    * feat: configure dynamic Django settings based on environment variable
    
    * fix: standardize environment variable naming in Docker configuration
    
    * fix: rename environment variable from ENV to ENVIRONMENT in Docker configuration
    
    * feat: update Docker configuration for development environment and fixed WSGI settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,12 +1,13 @@
 from .base import *
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
@@ -19,8 +20,8 @@ DEBUG = True
 FRONTEND_URL = 'http://localhost:4200'
 AWS_STORAGE_BUCKET_NAME = 'sinamecc-dev'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
+environment = os.getenv("ENVIRONMENT")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{environment}")
 application = get_wsgi_application()
